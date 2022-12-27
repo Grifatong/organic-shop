e7c1b98bce110d9bdc08e610c33c4dac18ed9d10
Grifatong <miroslav.grifatong@gmail.com>
2022-12-27 12:29:02 +0100

    add files

---
+++ js/index.js
@@ -0,0 +1,81 @@
+const searchForm = document.querySelector(".search-form");
+const shoppingCart = document.querySelector(".header .shopping-cart");
+const loginForm = document.querySelector(".login-form");
+const navbar = document.querySelector(".navbar");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+const swiper1 = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiper2 = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
